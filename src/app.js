4d7fa1cbd566702cff85be762551f041d6c723fa
thanhlong <info@coders.tokyo>
2020-08-14 16:44:21 +0700

    finsh project

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
-  }
+require('dotenv').config()
 const express = require("express")
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
